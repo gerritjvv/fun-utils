f2d3ee9705d160c05484228f0ce8b51bf7cb1827
gerritjvv <gerritjvv@gmail.com>
2013-12-30 23:50:41 +0100

    make start-channel non blocking by default with a wait switch

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fun-utils "0.2.1"
+(defproject fun-utils "0.2.2"
   :description "Clojure utility functions that come up time and again while developing clojure software"
   :url "https://github.com/gerritjvv/fun-utils"
   :license {:name "ECLIPSE PUBLIC LICENSE"
